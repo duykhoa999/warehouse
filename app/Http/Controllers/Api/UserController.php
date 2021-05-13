407f68b6f61d00e049e5b5b9f09c8060dc79e512
Duy Khoa <duykhoaptit@gmail.com>
2021-05-13 20:54:03 +0700

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -26,9 +26,9 @@ class UserController extends Controller
             ]);
         }
  
-        $credentials = request(['email', 'password']);
+        // $credentials = request(['email', 'password']);
  
-        if (!Auth::attempt($credentials)) {
+        if (!$request->authenticate()) {
             return response()->json([
                 'status' => 'fails',
                 'message' => 'Unauthorized'
