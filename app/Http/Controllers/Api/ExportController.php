9cf5ab7619fddd4c44d57c2d469d01189a64ba64
Duy Khoa <duykhoaptit@gmail.com>
2021-05-14 21:49:12 +0700

    Update ExportController.php

--- app/Http/Controllers/Api/ExportController.php
+++ app/Http/Controllers/Api/ExportController.php
@@ -32,6 +32,7 @@ class ExportController extends Controller
      */
     public function store(Request $request)
     {
+        dd("abx");
         $export = Export::create($request->all());
         dd($export);
         
