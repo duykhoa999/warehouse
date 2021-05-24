f7f58b1186f7a81da25c8ccc7231f8d80944c19d
Duy Khoa <duykhoaptit@gmail.com>
2021-05-24 12:40:46 +0700

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
     public function index()
     {
         $status = 1;
-        $data = User::paginate(1);
+        $data = User::paginate(10);
         if ($data->isEmpty()) {
             $status = -1;
             $message = "No Data";
