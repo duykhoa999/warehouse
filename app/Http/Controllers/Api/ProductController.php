e1e71e15c93670270f8d86734a63bc81ce58645f
Duy Khoa <duykhoaptit@gmail.com>
2021-07-15 15:41:50 +0700

    test

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -135,7 +135,7 @@ class ProductController extends Controller
             $status = -1;
             $message = "Cannot find this product!";
         }
-        else if (!empty($product->idetails)) {
+        else if ($product->idetails != null) {
             $status = -3;
             $message = "Delete Failed!";
         }
